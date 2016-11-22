e0155a89e279b2b00db6cd990332892643673cad
kulchenkodenis <hohllow@gmail.com>
2016-11-22 21:13:47 +0200

    Добавлены обычный и минимизированный код JS

--- js/search-form.js
+++ js/search-form.js
@@ -1,1 +1,9 @@
-var formlink=document.querySelector(".search-btn"),formblock=document.querySelector(".true-form");formlink.addEventListener("click",function(a){a.preventDefault(),formblock.classList.toggle("show-up")});
+var formlink = document.querySelector(".search-btn");
+var formblock = document.querySelector(".true-form");
+
+formlink.addEventListener("click", function(event) {
+  event.preventDefault();
+  formblock.classList.toggle("show-up");
+
+
+});
